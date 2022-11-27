5c438c22c255055d0fb731d02a080bf261b94ad9
TrixieL20 <minjung0768@gmail.com>
2022-11-27 11:31:39 +0900

    commit

--- calculater.py
+++ calculater.py
@@ -16,14 +16,10 @@ class Main(QDialog):
         layout_equation_solution = QFormLayout()
 
         ### 수식 입력과 답 출력을 위한 LineEdit 위젯을 하나의 창으로 생성
-        label_equation = QLabel("Equation: ")
-        label_solution = QLabel("Number: ")
         self.equation = QLineEdit("")
-        self.solution = QLineEdit("")
 
         ### layout_equation_solution 레이아웃에 수식, 답 위젯을 추가
-        layout_equation_solution.addRow(label_equation, self.equation)
-        layout_equation_solution.addRow(label_solution, self.solution)
+        layout_equation_solution.addRow(self.equation)
 
         ### 사칙연산 버튼 생성
         button_plus = QPushButton("+")
@@ -120,11 +116,10 @@ class Main(QDialog):
     def button_equal_clicked(self):
         equation = self.equation.text()
         solution = eval(equation)
-        self.solution.setText(str(solution))
+        self.equation.setText(str(solution))
 
     def button_clear_clicked(self):
         self.equation.setText("")
-        self.solution.setText("")
 
     def button_backspace_clicked(self):
         equation = self.equation.text()
